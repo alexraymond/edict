64067c4cf83dac72a9ab4693ce247b952f429138
Kier Dugan <kier.dugan@gmail.com>
2018-05-17 00:35:02 +0100

    Completed type-erasure classes for free and bound functions.

--- examples/hello-world/main.cpp
+++ examples/hello-world/main.cpp
@@ -1,15 +1,17 @@
 /*
-* Edict is a blackboard messaging system -- have fun!
-* Copyright (c) 2018 Alex Raymond, Kier Dugan.
-*/
+ * Edict is a blackboard messaging system -- have fun!
+ * Copyright (c) 2018 Alex Raymond, Kier Dugan.
+ */
 
 #include <edict/edict.h>
 using namespace edict;
 
 #include <iostream>
+#include <memory>
 using namespace std;
 
 
+// ================================================================================================
 void helloHandler(const string &message_)
 {
 	cout << message_ << ", Handler!" << endl;
@@ -21,12 +23,62 @@ void printer(const string &message_)
 }
 
 
+// ================================================================================================
+class DirectPrinter final
+{
+public:
+    explicit DirectPrinter(const string &name_) :
+        m_name(name_)
+    {
+    }
+
+    void print(const string &message_)
+    {
+        cout << "DirectPrinter(\"" << m_name << "\"): " << message_ << endl;
+    }
+
+    bool operator== (const DirectPrinter &other_) const
+    {
+        return m_name == other_.m_name;
+    }
+
+private:
+    const string m_name;
+};
+
+
+// ================================================================================================
+class IndirectPrinter final
+{
+public:
+    explicit IndirectPrinter(const string &name_) :
+        m_name(name_)
+    {
+    }
+
+    void print(const string &message_)
+    {
+        cout << "IndirectPrinter(\"" << m_name << "\"): " << message_ << endl;
+    }
+
+private:
+    const string m_name;
+};
+
+
+// ================================================================================================
 int main(int argc, char **argv)
 {
 	edict::Broadcaster broadcaster;
 
 	broadcaster.subscribe("/edict/hello", &helloHandler);
 	broadcaster.subscribe("/edict/hello", &printer);
+
+    DirectPrinter printer { "DotMatrix" };
+    broadcaster.subscribe("/edict/hello", printer, &DirectPrinter::print);
+
+    auto pronter = make_unique<IndirectPrinter>("OnkJot");
+    broadcaster.subscribe("/edict/hello", pronter.get(), &IndirectPrinter::print);
 
 	broadcaster.publish("/edict/hello", "Hello");
 
--- include/edict/Broadcaster.h
+++ include/edict/Broadcaster.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include "edict/Callable.h"
+
 #include <iostream>
 #include <map>
 #include <string>
@@ -16,11 +18,10 @@ namespace edict
 class Broadcaster final
 {
 public:
-	typedef void(*ReceiverFunc)(const std::string &);
-
 	Broadcaster() :
 		m_subscriptions()
-	{}
+	{
+    }
 
 	bool publish(const std::string &topic_, const std::string &data_)
 	{
@@ -32,44 +33,77 @@ public:
 		return true;
 	}
 
-	bool subscribe(const std::string &topic_, ReceiverFunc receiver_)
+	bool subscribe(const std::string &topic_, Callable::FreeReceiver receiver_)
 	{
-		auto subscription = make_pair(topic_, receiver_);
-		auto range = m_subscriptions.equal_range(topic_);
-		for (auto it = range.first; it != range.second; ++it)
-		{
-			if (it->second == receiver_)
-			{
-				std::cerr << "Attempting to double subscribe!" << std::endl;
-				return false;
-			}
-		}
-		m_subscriptions.insert(subscription);
-		return true;
+        return _subscribe(topic_, Callable(receiver_));
 	}
+    template <typename T>
+    bool subscribe(const std::string &topic_, T *object_,
+                   Callable::BoundReceiver<T *> receiver_)
+    {
+        return _subscribe(topic_, Callable(object_, receiver_));
+    }
+    template <typename T>
+    bool subscribe(const std::string &topic_, T &object_, Callable::BoundReceiver<T> receiver_)
+    {
+        return _subscribe(topic_, Callable(object_, receiver_));
+    }
 
-	bool unsubscribe(const std::string &topic_, ReceiverFunc receiver_)
-	{
-		unsigned removals = 0;
-		auto range = m_subscriptions.equal_range(topic_);
-		for (auto it = range.first; it != range.second;)
-		{
-			if (it->second == receiver_)
-			{
-				// Associative-container erase idiom
-				m_subscriptions.erase(it++);
-				++removals;
-			}
-			else
-			{
-				++it;
-			}
-		}
-		return removals > 0;
-	}
+    bool unsubscribe(const std::string &topic_, Callable::FreeReceiver receiver_)
+    {
+        return _unsubscribe(topic_, Callable(receiver_));
+    }
+    template <typename T>
+    bool unsubscribe(const std::string &topic_, T *object_,
+                     Callable::BoundReceiver<T *> receiver_)
+    {
+        return _unsubscribe(topic_, Callable(object_, receiver_));
+    }
+    template <typename T>
+    bool unsubscribe(const std::string &topic_, T &object_,
+                     Callable::BoundReceiver<T> receiver_)
+    {
+        return _unsubscribe(topic_, Callable(object_, receiver_));
+    }
 
 private:
-	std::multimap<std::string, ReceiverFunc> m_subscriptions;
+    bool _subscribe(const std::string &topic_, Callable receiver_)
+    {
+        auto subscription = make_pair(topic_, receiver_);
+        auto range = m_subscriptions.equal_range(topic_);
+        for (auto it = range.first; it != range.second; ++it)
+        {
+            if (it->second == receiver_)
+            {
+                std::cerr << "Attempting to double subscribe!" << std::endl;
+                return false;
+            }
+        }
+        m_subscriptions.insert(subscription);
+        return true;
+    }
+
+    bool _unsubscribe(const std::string &topic_, Callable receiver_)
+    {
+        unsigned removals = 0;
+        auto range = m_subscriptions.equal_range(topic_);
+        for (auto it = range.first; it != range.second;)
+        {
+            if (it->second == receiver_)
+            {
+                // Associative-container erase idiom
+                m_subscriptions.erase(it++);
+                ++removals;
+            }
+            else
+            {
+                ++it;
+            }
+        }
+        return removals > 0;
+    }
+
+	std::multimap<std::string, Callable> m_subscriptions;
 };
 
 }
