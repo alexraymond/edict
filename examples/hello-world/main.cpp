dcf12f8dd8d1e4f1125a20b3ad28e5832d635164
Alex Raymond <raymond.alex@gmail.com>
2018-05-23 18:22:22 +0000

    Typo

--- examples/hello-world/main.cpp
+++ examples/hello-world/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
     broadcaster.subscribe("/edict/hello", &helloHandler);
     broadcaster.subscribe("/edict/hello", &printer);
     broadcaster.subscribe(regex("(\\+|-)?[[:digit:]]+"), &helloHandler);
-    broadcaster.subscribe([](const string topic_) { return topic_.size() < 6; }, &printer);
+    broadcaster.subscribe([](const string &topic_) { return topic_.size() < 6; }, &printer);
 
     broadcaster.publish("/edict/hello", "Hello");
     broadcaster.publish("1234", "Bye");
